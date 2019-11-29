572df5c03abf392178b2eaa171b3981b313f1ff1
wzp <pan2869772090@163.com>
2019-11-29 18:05:38 +0800

    获取用户信息

---
+++ miniprogram/pages/login/login.js
@@ -0,0 +1,90 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avatarUrl: '/images/user-unlogin.png',
+    nickName: "用户未登陆",
+    city: "未知",
+  },
+  getUserInfomation: function (event) {
+    console.log('getUserInfomation打印的事件对象', event)
+    let { avatarUrl, city, nickName } = event.detail.userInfo
+    avatarUrl = avatarUrl.split("/")
+    avatarUrl[avatarUrl.length - 1] = 0;
+    avatarUrl = avatarUrl.join('/');
+    this.setData({
+      avatarUrl, city, nickName
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              let { avatarUrl, city, nickName } = res.userInfo
+              this.setData({
+                avatarUrl, city, nickName
+              })
+            }
+          })
+        }
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
